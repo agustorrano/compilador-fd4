75a2d756bf85b63bc40add82322665120c72de9a
Luciano Scola <lucianoluigiscola@gmail.com>
2024-09-26 15:06:01 -0300

    subst

--- src/CEK.hs
+++ src/CEK.hs
@@ -6,6 +6,7 @@ import Common
 
 import MonadFD4
 import Eval (semOp)
+import Subst (subst, subst2)
 
 type Env = [Val]
 
@@ -14,8 +15,8 @@ data Val =
   | VClos Clos
 
 data Clos =
-    VClosfun Name Ty Env TTerm
-  | VClosfix Name Ty Name Ty Env TTerm
+    VClosfun Env (Scope (Pos,Ty) Var)
+  | VClosfix Env (Scope2 (Pos,Ty) Var)
 
 instance (Show Val) where
   show (VNat (CNat n)) = show n
@@ -43,19 +44,19 @@ seek (V info var) e k =
           Just t -> seek t e k
           Nothing -> failPosFD4 (fst info) "Esto esta mal"
 seek (Const _ c) e k = destroy (VNat c) k
-seek (Lam _ n ty (Sc1 tt)) e k = destroy (VClos (VClosfun n ty e tt)) k
+seek (Lam _ n ty sc) e k = destroy (VClos (VClosfun e sc)) k
 seek (App _ t1 t2) e k = seek t1 e (KArg e t2:k) 
 seek (Print _ str t1) e k = seek t1 e (KPrint str:k)
 seek (BinaryOp _ op t1 t2) e k = seek t1 e (KBopt e op t2:k)
-seek (Fix _ f ty1 x ty2 (Sc2 tt)) e k = destroy (VClos (VClosfix f ty1 x ty2 e tt)) k 
+seek (Fix _ f ty1 x ty2 sc2) e k = destroy (VClos (VClosfix e sc2)) k 
 seek (IfZ _ t1 i d) e k = seek t1 e (KIfz e i d:k)
 seek (Let _ _ _ t (Sc1 t')) e k = seek t e (KLet e t':k)
 
 destroy :: MonadFD4 m => Val -> Kont -> m Val
 destroy v [] = do return v
 destroy (VClos c) (KArg e t:k) = seek t e (KClos c:k)
-destroy v (KClos (VClosfun _ _ e t):k) = seek t (v:e) k
-destroy v (KClos l@(VClosfix _ _ _ _ e t):k) = seek t (v:VClos l:e) k
+destroy v (KClos (VClosfun e (Sc1 t)):k) = seek t (v:e) k
+destroy v (KClos l@(VClosfix e (Sc2 t)):k) = seek t (v:VClos l:e) k
 destroy v (KPrint s:k) = do {printFD4 $ s ++ show v; destroy v k}
 destroy v (KLet e t:k) = seek t (v:e) k
 destroy n (KBopt e op t:k) = seek t e (KBopv n op:k)
@@ -74,7 +75,23 @@ trash :: (Pos, Ty)
 trash = (NoPos,NatTy)
 
 -- TODO : TERMINAR :P
+
+ltransform :: MonadFD4 m => [Val] -> m [TTerm]
+ltransform [] = do return []
+ltransform (v:vs) = 
+  do 
+    t <- transform v 
+    ts <- ltransform vs
+    return $ t:ts
+
 transform :: MonadFD4 m => Val -> m TTerm
 transform (VNat c) = do return  (Const trash c) 
-transform (VClos (VClosfun x ty e t)) = do return (Lam trash x ty (Sc1 t))
-transform (VClos (VClosfix f ty1 x ty2 e t)) = do return (Fix trash f ty1 x ty2 (Sc2 t))
+transform (VClos (VClosfun [v] sc1)) =
+  do
+    t <- transform v
+    return $ subst t sc1
+transform (VClos (VClosfix [v, f] sc2)) =
+  do
+    t <- transform v
+    tf <- transform f
+    return $ subst2 tf t sc2
